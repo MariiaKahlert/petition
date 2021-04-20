3f02569c49f057b11f4131e351c270f14ba99d29
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-04-20 16:48:07 +0200

    structures petition page and start styling it

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const { getCities, addCity } = require("./db");
+const {} = require("./db");
+
+// Require express
 const express = require("express");
 const app = express();
 
+// Require express-handlebars
+const hb = require("express-handlebars");
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
+
+// Serve static files from public folder
 app.use(express.static("public"));
+
+app.get("/petition", (req, res) => {
+    res.render("petition", {
+        layout: "main",
+    });
+});
 
 app.listen(8080, () => console.log("Server listening on port 8080"));
