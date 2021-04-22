27c4126ea963001d97af88dba1ae90d3e2c2f724
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-04-22 12:57:07 +0200

    creates users table and modifies signatures one

--- setup.sql
+++ setup.sql
@@ -1,9 +1,18 @@
 DROP TABLE IF EXISTS signatures;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id            SERIAL PRIMARY KEY,
+    first_name    VARCHAR NOT NULL CHECK (first_name != ''),
+    last_name     VARCHAR NOT NULL CHECK (last_name != ''),
+    email         VARCHAR NOT NULL UNIQUE CHECK (email != ''),
+    password_hash VARCHAR NOT NULL CHECK (password_hash != ''),
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE signatures (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR NOT NULL CHECK (first_name != ''),
-    last_name VARCHAR NOT NULL CHECK (last_name != ''),
-    signature VARCHAR NOT NULL CHECK (signature != ''),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    user_id    INTEGER NOT NULL UNIQUE REFERENCES users (id),
+    signature TEXT NOT NULL CHECK (signature != ''),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
