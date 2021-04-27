7382fa2fd4b0d1be962438cdc1a2e3a0da98a008
Mariia Kahlert <mariia.kahlert@gmail.com>
2021-04-27 13:13:58 +0200

    writes petition.js module

---
+++ routers/petition.js
@@ -0,0 +1,30 @@
+const { app } = require("../server");
+const { getSignature, signPetition } = require("../db");
+
+app.get("/petition", (req, res) => {
+    getSignature(req.session.userId).then((result) => {
+        if (result.rows.length === 0) {
+            res.render("petition", {
+                layout: "main",
+            });
+            return;
+        }
+        res.redirect("/thanks");
+    });
+});
+
+app.post("/petition", (req, res) => {
+    const { userId } = req.session;
+    const { signature } = req.body;
+    signPetition(userId, signature)
+        .then(() => {
+            res.redirect("/thanks");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.render("petition", {
+                layout: "main",
+                error: "Something went wrong. Please, try to sign again.",
+            });
+        });
+});
--- server.js
+++ server.js
@@ -112,33 +112,7 @@ app.use("/profile", profileRouter);
 
 // Petition
 
-app.get("/petition", (req, res) => {
-    getSignature(req.session.userId).then((result) => {
-        if (result.rows.length === 0) {
-            res.render("petition", {
-                layout: "main",
-            });
-            return;
-        }
-        res.redirect("/thanks");
-    });
-});
-
-app.post("/petition", (req, res) => {
-    const { userId } = req.session;
-    const { signature } = req.body;
-    signPetition(userId, signature)
-        .then(() => {
-            res.redirect("/thanks");
-        })
-        .catch((err) => {
-            console.log(err);
-            res.render("petition", {
-                layout: "main",
-                error: "Something went wrong. Please, try to sign again.",
-            });
-        });
-});
+require("./routers/petition");
 
 // Thanks
 
